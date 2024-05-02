6aba53c255949f0d2ae84073307e416c59acb1a2
Mrhamza01 <hamza.kamboh035@gmail.com>
2024-05-02 19:03:30 +0500

    follow table update

--- database/migrations/2024_04_06_083729_create_follows_table.php
+++ database/migrations/2024_04_06_083729_create_follows_table.php
@@ -21,7 +21,7 @@ return new class extends Migration {
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('target_id')->references('id')->on('users')->onDelete('cascade');
-            $table->check('user_id != target_id');
+          
             $table->unique(['user_id', 'target_id']);
         });
 
